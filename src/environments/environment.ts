6616138072a28f7ddc7d20734a4285ae98b3691f
Fabio Torres <fabioa.torresc@utadeo.edu.coionic>
2020-03-14 14:45:05 -0500

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+
+  apiKey: 'AIzaSyB6C8bzwH1O7gSwFThCS0sLyaKk9htbrbs',
+  authDomain: 'appwc-25317.firebaseapp.com',
+  databaseURL: 'https://appwc-25317.firebaseio.com',
+  projectId: 'appwc-25317',
+  storageBucket: 'appwc-25317.appspot.com',
+  messagingSenderId: '153604294753',
+  appId: '1:153604294753:web:58674d8f102d000c420220'
+  }
 };
 
 /*
